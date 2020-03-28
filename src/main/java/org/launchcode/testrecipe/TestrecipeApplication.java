d9c1fe1ed881bb791c3268fa7d46d356964705b3
Laa <lastembo@gmail.com>
2020-03-28 15:21:27 -0500

    initial build testing pagination

---
+++ src/main/java/org/launchcode/testrecipe/TestrecipeApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.testrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestrecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestrecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/testrecipe/TestrecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.testrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestrecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
